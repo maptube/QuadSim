a6205d84fd57c703db92ae172f1474d0e16746ca
richard <richard.milton@ucl.ac.uk>
2016-02-04 21:58:30 +0000

    simple aile p pid panel slider working

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	//UI code for main menu to detect when X button is pressed and trigger opening the main menu
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("SpeedlinkButtonX")) {
+			//Debug.Log ("BUTTON X");
+			GameObject goSCM = GameObject.Find("ScreenManager");
+			ScreenManager scm = (ScreenManager)goSCM.GetComponent(typeof(ScreenManager));
+			GameObject goMainMenu = GameObject.Find("MainMenuPanel");
+			//Panel MainMenuPanel = MainMenu.GetComponentInChildren<Panel>();
+			//ScreenManager scm = (ScreenManager)goSCM.GetComponent(typeof(Animator));
+			Animator MainMenuAnim = goMainMenu.GetComponent<Animator>();
+			scm.OpenPanel(MainMenuAnim);
+		}
+	
+	}
+
+	//void OnGUI() {
+	//}
+}
---
+++ Assets/PIDPanelScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class PIDPanelScript : MonoBehaviour {
+	GameObject quad;
+	QuadcopterBehaviourScript script;
+
+	// Use this for initialization
+	void Start () {
+		//GameObject quad = GameObject.Find (/*"testquadcopter2"*/"F450");
+		quad = GameObject.Find ("F450");
+		script = quad.GetComponent<QuadcopterBehaviourScript> ();
+		//set initial values here...
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void AileronPChange(float P) {
+		script.setAileronP (P);
+	}
+
+}
