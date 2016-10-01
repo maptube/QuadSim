b03a837a1a1fd55f2f6db365c4f0aa5efbcef73a
richard <richard.milton@ucl.ac.uk>
2016-10-01 19:37:36 +0100

    Fixed zero thrust problem and tidied quadcopter behaviour script

--- Assets/Scripts/QuadcopterBehaviourScript.cs
+++ Assets/Scripts/QuadcopterBehaviourScript.cs
@@ -89,22 +89,23 @@ public class QuadcopterBehaviourScript : MonoBehaviour {
 
 	//Quadcopter physical data (Diatone Blue Ghost)
 	float Mass = 0.48f; //KG
-	//propeller (TODO: add efficiency?)
-	float PropPitch = 0.1f; //inches
-	float PropDiameter = 0.1f; //inches
-	//motor parameters
-	float Kv = 2280.0f; //from the emax box - max thrust is 460g
-	float I0 = 0.6f; //I0 and Rs from Internet data, I0 also given as 0.3A
-	float Rs = 0.117f; //fron Internet - looks high?
-	float VBatt=11.1f; //nominally 3S LiPo
-	//physical dimensions
-	float LArm = 0.125f; //moment arm from CG to prop centre in metres
-	//other motor consts related to thrust and moment based on rotational speed
-	float Ixx=0.002f, Iyy=0.002f, Izz=0.004f, Izx=0f; //moments of inertia - these are wrong, should be at least times 10
-	//Drag?
+    //propeller (TODO: add efficiency?)
+    //float PropPitch = 0.1f; //inches
+    //float PropDiameter = 0.1f; //inches
+    //motor parameters
+    //float Kv = 2280.0f; //from the emax box - max thrust is 460g
+    //float I0 = 0.6f; //I0 and Rs from Internet data, I0 also given as 0.3A
+    //float Rs = 0.117f; //fron Internet - looks high?
+    //float VBatt=11.1f; //nominally 3S LiPo
+    //physical dimensions
+    //float LArm = 0.125f; //moment arm from CG to prop centre in metres
+    //other motor consts related to thrust and moment based on rotational speed
+    //float Ixx=0.002f, Iyy=0.002f, Izz=0.004f, Izx=0f; //moments of inertia - these are wrong, should be at least times 10
+    //Drag?
+    float MaxThrust = 10.0f; //Maximum thrust from all four motors - just set this rather than messing about with the motor constants above
 
-	//Control Rates
-	const float AxisRateRoll = 45.0f; //this is how many degrees tilt you get for full stick deflection
+    //Control Rates
+    const float AxisRateRoll = 45.0f; //this is how many degrees tilt you get for full stick deflection
 	const float AxisRatePitch = 45.0f;
 	const float AxisRateYaw = 4.0f;
 	//PID parameters	
@@ -112,8 +113,15 @@ public class QuadcopterBehaviourScript : MonoBehaviour {
 	const float PID_Elevator_P = 0.5f, PID_Elevator_I = 0.5f, PID_Elevator_D = 0.001f;
 	const float PID_Rudder_P = 0.6f, PID_Rudder_I = 0.4f, PID_Rudder_D = 0.0f;
 
+    float Thrust = 0; //total thrust from all four props in Newtons
+
 	//calculated values which are saved
-	float MaxThrust = 0; //you could just set this rather than messing about with the motor
+
+    //thrust required to hover and joystick multiplier (calculated)
+    private float HoverThrust = 0;
+    //These are the constants for a two part throttle curve split on the hoverThrust at the joystick mid point.
+    private float ThrustConstantLow = 0; //multiplier used with HoverThrust to set hover at joystick centre (t=0), zero at joystick min (t=-1) and max thrust at joystick max (t=+1)
+    private float ThrustConstantHigh = 0; //same as above
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//PID Controllers for aileron, elevator, rudder and altitude. Associated angles.
@@ -136,13 +144,12 @@ public class QuadcopterBehaviourScript : MonoBehaviour {
 	bool AltitudeHoldModeEnabled = false;
 	float AltitudeHold = 0; //altitude to hold to
 	float aileron, elevator, rudder, throttle; //read from the joysitck [-1..+1]
-	//float txRollAngle, txPitchAngle, txYawRate; //desired roll, pitch and yaw angles or rates derived fron the transmitter stick positions and max angles
-	//float bodyRollAngle, bodyPitchAngle, bodyYawRate; //gyro sensor values, measured from the aircraft matrix, used as PID inputs
 
+    //simulated MEMs sensor angles and rates
 	Vector3 gyroAngles;
 	Vector3 gyroRates;
 
-	float E0,E1,E2,E3; //ESC inputs
+	//float E0,E1,E2,E3; //ESC inputs
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//Transmitter and control data
@@ -231,8 +238,10 @@ public class QuadcopterBehaviourScript : MonoBehaviour {
 		           + "RollAngle: "+gyroAngles.x+" Target: "+aileron*AxisRateRoll+"\n"
 		           +"E: "+elevator+"\n"
 		           +"R: "+rudder+"\n"
+                   +"T: "+throttle+"\n"
 		           +"YawRateTarget: "+rudder*AxisRateYaw+" GyroRate: "+gyroRates.z+"\n"
 		           +"gyroRates: "+gyroRates.x.ToString ("N2")+" "+gyroRates.y.ToString ("N2")+" "+gyroRates.z.ToString ("N2")+"\n"
+                   +"thrust: "+Thrust+"\n"
 		);
 	}
 
@@ -242,25 +251,24 @@ public class QuadcopterBehaviourScript : MonoBehaviour {
 
 		// Change the mass of the object's Rigidbody.
 		rb.mass = this.Mass; //1.0f;
-		rb.drag = 0.8f;
+		rb.drag = 0.4f; //was 0.8
 		rb.angularDrag = 3.5f; //0.0001f; //4.5f; //or inertiaTensor? what units is this in?
 		rb.centerOfMass = new Vector3 (0, 0, 0);
-		//rb.inertiaTensor = new Vector3 (Ixx,Iyy,Izz); /*new Vector3 (1.0f,1.0f,1.0f);*/
+        //rb.inertiaTensor = new Vector3 (Ixx,Iyy,Izz); /*new Vector3 (1.0f,1.0f,1.0f);*/
 
-		//create an electric motor model from the parameters and determine its max thurst from static RPM
-		ElectricMotorScript EMotor = new ElectricMotorScript (Kv, I0, Rs, VBatt);
-		float StaticRPM = EMotor.SolveStaticRPM (PropDiameter, PropPitch);
-		this.MaxThrust = 5.0f; //EMotor.CalcThrust (PropDiameter, PropPitch, StaticRPM);
-	}
+        //create an electric motor model from the parameters and determine its max thurst from static RPM
+        //ElectricMotorScript EMotor = new ElectricMotorScript (Kv, I0, Rs, VBatt);
+        //float StaticRPM = EMotor.SolveStaticRPM (PropDiameter, PropPitch);
+        //this.MaxThrust = 5.0f; //EMotor.CalcThrust (PropDiameter, PropPitch, StaticRPM);
+
+        //calculate thrust required to hover and make sure throttle centre equates to this thrust level (t=0), while t=-1 gives zero thrust and t=+1 gives max
+        this.HoverThrust = this.Mass * 9.81f;
+        this.ThrustConstantLow = this.HoverThrust;
+        this.ThrustConstantHigh = (this.MaxThrust - this.HoverThrust);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		//float speed = 1.0f;
-		//float distance = 1.0f; //speed * Time.deltaTime * Input.GetAxis("Horizontal");
-		//transform.Translate(Vector3.right * distance);
-		//Rigidbody rb = GetComponent<Rigidbody>();
-		//transform.Translate (rb.velocity);
-
 		//UI stuff
 		if (Input.GetButtonDown ("SpeedlinkButtonB")) {
 			ToggleAltHold ();
@@ -313,20 +321,11 @@ public class QuadcopterBehaviourScript : MonoBehaviour {
 
 		//Altitude hold controller - override the throttle
 		if (AltitudeHoldModeEnabled) {
-//			throttle = pidAltitude.process(rb.position.y,AltitudeHold,Time.deltaTime);
+			throttle = pidAltitude.process(rb.position.y,AltitudeHold,Time.deltaTime);
 		}
 
-		//convert to requested angles etc
-		//float thrust = 10.0f+throttle*6.0f; //t*5.0
-		//float thrust = 4.8f+throttle*6.0f;
-		//ailerons and elevator set requested angles to horizontal, rudder sets rotation speed
-		//Convert transmitter control inputs to desired roll, pitch, yaw angles or rates
-		//txRollAngle = -aileron * MaxRollAngle; //degrees - TODO: need to fix axis problem here
-		//txPitchAngle = -elevator * MaxPitchAngle; //degrees (40)
-		//txYawRate = rudder * MaxYawRate; //degrees per second (?) (24)
-
 		//Simulate gyro sensor on the aircraft by measuring current body frame angles
-		Vector3 euler = rb.transform.localEulerAngles; //was eulerAngles, but localEulerAngles seems better?
+		Vector3 euler = rb.transform.localEulerAngles;
 		//roll angle (degrees)
 		gyroAngles.x = euler.z;
 		if (gyroAngles.x > 180.0f)
@@ -340,95 +339,85 @@ public class QuadcopterBehaviourScript : MonoBehaviour {
 		//note no absolute yaw angle
 
 		//now body angle rates
-		//roll rate (degrees per sec)
-		//Vector2 relAV = rb.angularVelocity * rb.transform.forward;
-		Vector3 relAV = transform.InverseTransformDirection(rb.angularVelocity);
-		//gyroRates.x = -rb.angularVelocity.z;
-		gyroRates.x = -relAV.z;
-		//pitch rate
-		//gyroRates.y = -rb.angularVelocity.x;
-		gyroRates.y = -relAV.x;
-		//yaw rate
-		//gyroRates.z = rb.angularVelocity.y;
-		gyroRates.z = relAV.y;
+		Vector3 relAV = transform.InverseTransformDirection(rb.angularVelocity); //need body rates, not world
+        gyroRates.x = -relAV.z; //roll rate (degrees per sec)
+        gyroRates.y = -relAV.x; //pitch rate
+        gyroRates.z = relAV.y; //yaw rate
 
-		//convert angles to radians - why aren't they radians already?
-		//bodyRollAngle = bodyRollAngle * Mathf.PI / 180.0f;
-		//bodyPitchAngle = bodyRollAngle * Mathf.PI / 180.0f;
-		//bodyRollRate = bodyRollRate * Mathf.PI / 180.0f;
-		//bodyPitchRate = bodyPitchRate * Mathf.PI / 180.0f;
-		//bodyYawRate = bodyYawRate * Mathf.PI / 180.0f;
-
-		float YawRateTarget = rudder * AxisRateYaw;
+        float YawRateTarget = rudder * AxisRateYaw;
 		float errorAngle;
 		//roll axis - it's not entirely clear how you transform an angle error into a target roll rate and put it into the PID controller.
 		//I'm using the P value for the controller here as it works well i.e. the linear bit of the controller.
-		//TODO: check how the Cleanflight P8[PIDLEVEL] constant.
+		//TODO: check how the Cleanflight P8[PIDLEVEL] constant works, I've more or less copied that code.
 		errorAngle = aileron * AxisRateRoll - gyroAngles.x;
 		float RollRateTarget = errorAngle * PID_Aileron_P; //hacked 0.4 - P8[PIDLEVEL]?????
 		//pitch axis
 		errorAngle = elevator * AxisRatePitch - gyroAngles.y;
 		float PitchRateTarget = errorAngle * PID_Elevator_P; //hacked 0.4f;
 
-
-		//PID Calculations using errors
-		//float P = pidAileron.process (txRollAngle - rollAngle, Time.deltaTime);
-		//float Q = pidElevator.process (txPitchAngle - pitchAngle, Time.deltaTime);
-		//float R = pidRudder.process (txYawRate - yawRate, Time.deltaTime);
-		//PID Calculations using actual, desired which the error is calculated from
+		//PID Calculations using (actual, desired) which the error is calculated from
 		float P = pidAileron.process (gyroRates.x, RollRateTarget, Time.deltaTime);
 		float Q = pidElevator.process (gyroRates.y, PitchRateTarget, Time.deltaTime);
 		float R = pidRudder.process (gyroRates.z, YawRateTarget, Time.deltaTime);
 
-		//*amount * Time.deltaTime???? in force mode
+        //*amount * Time.deltaTime???? in force mode
 
-		//PID then go into mixer to command motors
-		//Need E0,E1,E2,E3
-		// E1 E0
-		// E2 E3
-		//Clockwise: E1,E3 Anticlockwise: E0,E2
-		//these then get me F0,F1,F2,F3 (forces) and M0,M1,M2,M3 (torques)
-		//float Hover = 0.1f;
-		//E0 = eazSpeedOutput+Hover; E1 = eazSpeedOutput+Hover; E2 = eazSpeedOutput+Hover; E3 = eazSpeedOutput+Hover;
-		//E1 += pitchRate / 2; E0 += pitchRate / 2;
-		//E2 -= pitchRate / 2; E3 -= pitchRate / 2;
-		//
-		//E1 += rollRate / 2; E0 -= rollRate / 2;
-		//E2 += rollRate / 2; E3 -= rollRate / 2;
-		//
-		//E1 += yawRate / 2; E0 -= yawRate / 2;
-		//E2 -= yawRate / 2; E3 += yawRate / 2;
+        //Work out thrust from stick position. We're using a two part throttle curve split on the hover point at the mid throttle position.
+        //t=-1 => zero thrust, t=0 => HoverThrust, t=1 => MaxThrust
+        if (throttle < 0) Thrust = throttle * ThrustConstantLow + HoverThrust;
+        else Thrust = throttle * ThrustConstantHigh + HoverThrust;
 
-		//clamping - normalisation
-		//float EMax = Mathf.Max (E3, Mathf.Max (E2, Mathf.Max (E1, E0)));
-		//if (EMax>1.0f)
-		//{
-		//	//or subtract the delta?
-		//	E0/=EMax; E1/=EMax; E2/=EMax; E3/=EMax;
-		//}
-		//if (E0 < 0)
-		//	E0 = 0;
-		//if (E1 < 0)
-		//	E1 = 0;
-		//if (E2 < 0)
-		//	E2 = 0;
-		//if (E3 < 0)
-		//	E3 = 0;
+        //these are force and torque direct from the PID without modelling the four props, motors and mixer
+        rb.AddRelativeForce(new Vector3(0, Thrust, 0));
+        rb.AddRelativeTorque(new Vector3(-Q, R, -P));
 
-		//these are the working ones direct from PID
-		rb.AddRelativeForce (new Vector3 (0, throttle*MaxThrust*1+4.8f, 0));
-		rb.AddRelativeTorque (new Vector3 (-Q, R, -P));
+        //This is the mixer code for simulating the effect of the thrust from the four motors - removed in favour of just using the angles above.
+        //I might put this back in as an experiment, but a simulator doesn't need to simulator the four motors to then calculate torques as we can
+        //get the torques direct from the PID. It might be a more accurate simulation as you could model the motor delays and inertia as well as
+        //prop effects, but it might not work that well with the flight dynamics.
+        //PID then go into mixer to command motors
+        //Need E0,E1,E2,E3
+        // E1 E0
+        // E2 E3
+        //Clockwise: E1,E3 Anticlockwise: E0,E2
+        //these then get me F0,F1,F2,F3 (forces) and M0,M1,M2,M3 (torques)
+        //float Hover = 0.1f;
+        //E0 = eazSpeedOutput+Hover; E1 = eazSpeedOutput+Hover; E2 = eazSpeedOutput+Hover; E3 = eazSpeedOutput+Hover;
+        //E1 += pitchRate / 2; E0 += pitchRate / 2;
+        //E2 -= pitchRate / 2; E3 -= pitchRate / 2;
+        //
+        //E1 += rollRate / 2; E0 -= rollRate / 2;
+        //E2 += rollRate / 2; E3 -= rollRate / 2;
+        //
+        //E1 += yawRate / 2; E0 -= yawRate / 2;
+        //E2 -= yawRate / 2; E3 += yawRate / 2;
 
-		//new forces and moments
-		//calculate net force and torgue from all motors
-		//float F0 = MaxThrust * E0, F1 = MaxThrust * E1, F2 = MaxThrust * E2, F3 = MaxThrust * E3;
-		//rb.AddRelativeForce (0,F0 + F1 + F2 + F3,0);
-		//rb.AddForce(new Vector3 (0, F0 + F1 + F2 + F3, 0));
-		//float P = ((F1 + F2) - (F0 + F3)) * LArm; //it's not quite LArm as that's the hpot and we want the two other sides of the tri, but this will do
-		//float Q = 0; //((F1 + F0) - (F2 + F3)) * LArm;
-		//float R = 0; //((E0 + E2) - (E1 + E3)) * 0.1f; //TODO: need torque constant for revs E0..3
-		//rb.AddRelativeTorque (new Vector3 (Q, R, -P));
-	}
+        //clamping - normalisation
+        //float EMax = Mathf.Max (E3, Mathf.Max (E2, Mathf.Max (E1, E0)));
+        //if (EMax>1.0f)
+        //{
+        //	//or subtract the delta?
+        //	E0/=EMax; E1/=EMax; E2/=EMax; E3/=EMax;
+        //}
+        //if (E0 < 0)
+        //	E0 = 0;
+        //if (E1 < 0)
+        //	E1 = 0;
+        //if (E2 < 0)
+        //	E2 = 0;
+        //if (E3 < 0)
+        //	E3 = 0;
+
+        //new forces and moments
+        //calculate net force and torgue from all motors
+        //float F0 = MaxThrust * E0, F1 = MaxThrust * E1, F2 = MaxThrust * E2, F3 = MaxThrust * E3;
+        //rb.AddRelativeForce (0,F0 + F1 + F2 + F3,0);
+        //rb.AddForce(new Vector3 (0, F0 + F1 + F2 + F3, 0));
+        //float P = ((F1 + F2) - (F0 + F3)) * LArm; //it's not quite LArm as that's the hpot and we want the two other sides of the tri, but this will do
+        //float Q = 0; //((F1 + F0) - (F2 + F3)) * LArm;
+        //float R = 0; //((E0 + E2) - (E1 + E3)) * 0.1f; //TODO: need torque constant for revs E0..3
+        //rb.AddRelativeTorque (new Vector3 (Q, R, -P));
+    }
 
 	//public void OnTriggerEnter(Collider other) {
 	//	Debug.Log ("Quadcopter COLLIDE");
